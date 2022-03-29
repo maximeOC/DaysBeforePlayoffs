f2285cde615c1d1f178cfa10f9bd5730ce065bca
Maxime GASSPARD <maxime1513@gmail.com>
2022-03-29 23:00:39 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+const daysPlayoffs = '10 may 2022';
+
+function countdown() {
+
+    const daysplayoffsdate = new Date(daysPlayoffs);
+    const currentdate = new Date();
+
+    const totalseconds = (daysplayoffsdate - currentdate) / 1000;
+ 
+    const days = Math.floor (totalseconds / 3600 / 24);
+    const hours = Math.floor (totalseconds / 3600) % 24;
+    const mins = Math.floor(totalseconds / 60) % 60;
+    const seconds = Math.floor (totalseconds)  % 60 ;
+
+     daysEl.innerHTML = days;
+     hoursEl.innerHTML = hours;
+     minsEl.innerHTML = mins;
+     secondsEl.innerHTML = seconds;
+
+     console.log(days, hours, mins, seconds);
+    
+     
+}
+
+countdown();
+
+setInterval(countdown, 1000);
